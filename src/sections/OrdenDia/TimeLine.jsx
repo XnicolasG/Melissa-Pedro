2b86e19931819d80fc18578f06c3389f96751464
Nicolás García <nicolasg.ngl@gmail.com>
2024-09-18 23:27:01 -0500

    fixing errors in tailwind classes

--- src/sections/OrdenDia/TimeLine.jsx
+++ src/sections/OrdenDia/TimeLine.jsx
@@ -53,7 +53,7 @@ export const TimeLine = () => {
 
               <li key={index} class="relative w-20 md:w-28 lg:w-36 mb-6 sm:mb-0 flex-1 ">
                 <div class="flex items-center ">
-                  <div class={`bounce bounce-${index + 1} z-10 flex items-center justify-center size-10  rounded-full ring-0 ring-white bg-emerald-[#96A480] sm:ring-8 dark:ring-emerald-900 shrink-0`}>
+                  <div class={`bounce bounce-${index + 1} z-10 flex items-center justify-center size-10  rounded-full ring-0 ring-white dark:bg-[#96A480] sm:ring-8 dark:ring-emerald-900 shrink-0`}>
                     <p className='text-xl'>{item.icon}</p>
                   </div>
                   {

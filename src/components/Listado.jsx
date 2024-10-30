50a730c488eccd7e8163c7f3323b60ec434a1760
Nicolás García <nicolasg.ngl@gmail.com>
2024-10-29 22:51:47 -0500

    fixing attendace info, and UI styles fo table in Listado component

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -3,23 +3,28 @@ import { useGetData } from '../sections/Listado/hooks/useGetData';
 
 export const Listado = () => {
     const { list } = useGetData()
-    const [state, setState] = useState({
-        
-    })
+
+    const [displayList, setDisplayList] = useState([])
     const [isAttending, setIsAttending] = useState([])
     useEffect(() => {
-        list &&
-            setIsAttending(list.filter(it => it.attending !== false)) /*&& it.registration_type === 'grupo')*/
-    }, [list])
+        if (list.length > 0 || displayList.length === 0) {
+            setDisplayList(list);
+            setIsAttending(displayList
+                .filter(it => it.attending !== false)
+                .reduce((acc, item) => acc + item.number_of_people,0));
+        }
+    }, [list, displayList.length])
 
-    console.log(isAttending.length);
+    // const attending = isAttending
+    console.log(isAttending);
+    
 
     return (
-        <section className='px-6 w-full mt-28 flex flex-col items-center justify-center'>
-            <main className='w-[95%] md:w-[70%]'>
+        <section className='px-2 md:px-6 w-full mt-28 flex flex-col items-center justify-center'>
+            <main className='w-full md:w-[70%]'>
 
                 <h1 className='text-3xl text-center py-1 text-amber-50 bg-emerald-800 w-full rounded-t-lg'>Lista de Registros</h1>
-                <section className='flex justify-around'>
+                <section className='flex flex-col-reverse md:flex-row justify-around'>
 
                     <div className='w-full px-2 my-4 gap-2 flex flex-col items-start'>
                         <p className='text-gray-500 text-2xl'>Tipo de registro</p>
@@ -30,54 +35,57 @@ export const Listado = () => {
                         </div>
                     </div>
                     <div>
-                        <div className='flex flex-col items-center'>
-                        <p>
-                            Asistentes
-                        </p>
-                        <h2 class=" py-1 sm:py-2 text-center tracking-wider text-md sm:text-2xl bg-emerald-800  text-emerald-800  font-bold shadow-sm px-2 rounded-xl border-2 border-emerald-800
-                h-10 w-10 md:rounded-full md:flex md:justify-center md:items-center  md:backdrop-blur-sm md:bg-white/20 hover:scale-110 md:hover:bg-stone-200 md:hover:text-red-600 md:hover:border-red-600  transition duration-300 ">
-                            {isAttending.length}
-                        </h2>
-                    </div>
+                        <div className='flex flex-col px-4 md:items-center'>
+                            <p>
+                                Asistentes
+                            </p>
+                            <h2 className=" py-1 sm:py-2 text-center tracking-wider text-md sm:text-2xl text-emerald-800  font-bold shadow-sm px-2 border-2 border-emerald-800
+                                h-10 w-10 rounded-full md:flex md:justify-center md:items-center md:bg-white/20 hover:scale-110  transition duration-300 ">
+                                {isAttending}
+                            </h2>
+                        </div>
                     </div>
                 </section>
-                <table className='w-full '>
-                    <thead className='text-amber-50'>
-                        <tr className=''>
-                            <th className='border border-white' scope="col">Asistencia</th>
-                            <th className='border border-white' scope="col">Nombre</th>
-                            <th className='border border-white' scope="col">Tipo registro</th>
-                            <th className='border border-white' scope="col">P. registradas</th>
-                            <th className='border border-white' scope="col">Platos carne</th>
-                            <th className='border border-white' scope="col">Platos veg</th>
-                            <th className='border border-white' scope="col">Descripción</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <section className='overflow-x-scroll'>
+                    <table className='w-full '>
+                        <thead className='text-amber-50'>
+                            <tr className=''>
+                                <th className='border border-white' scope="col">Asistencia</th>
+                                <th className='border border-white' scope="col">Nombre</th>
+                                <th className='border border-white' scope="col">Tipo registro</th>
+                                <th className='border border-white' scope="col">P. registradas</th>
+                                <th className='border border-white' scope="col">Platos carne</th>
+                                <th className='border border-white' scope="col">Platos veg</th>
+                                <th className='border border-white' scope="col">Descripción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                        {
-                            list.map((item) => (
-                                <tr
-                                    key={item.id}
-                                    className='text-center odd:bg-white even:bg-emerald-600/50'
-                                >
-                                    <td>{item.attending ? 'Si' : 'No'}</td>
-                                    <td>{item.names}</td>
-                                    <td>{item.registration_type}</td>
-                                    <td>{item.number_of_people}</td>
-                                    {item.TMeals?.map((plate, index) => (
-                                        <React.Fragment key={index}>
-                                            <td>{plate.meat_count}</td>
-                                            <td>{plate.vegetarian_count}</td>
-                                        </React.Fragment>
-                                    ))
-                                    }
-                                    <td>{item.description}</td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+                            {
+                                displayList.map((item) => (
+                                    <tr
+                                        key={item.id}
+                                        className='text-center odd:bg-white even:bg-emerald-600/50'
+                                    >
+                                        <td>{item.attending ? 'Si' : 'No'}</td>
+                                        <td>{item.names}</td>
+                                        <td>{item.registration_type}</td>
+                                        <td>{item.number_of_people}</td>
+                                        {item.TMeals?.map((plate, index) => (
+                                            <React.Fragment key={index}>
+                                                <td>{plate.meat_count}</td>
+                                                <td>{plate.vegetarian_count}</td>
+                                            </React.Fragment>
+                                        ))
+                                        }
+                                        <td>{item.description}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </section>
+
             </main>
         </section>
     )
